a235821cf731ddf0b17c5dbd0e2c420f539923ed
Ashley Wooten <ashley.cummings.33.ac@gmail.com>
2021-10-11 14:45:05 -0500

    Loops Exercise

---
+++ script.js
@@ -0,0 +1,33 @@
+// Loops Exercise
+
+// 1
+for (i = 1; i <=7; i++){
+console.log(i);
+}
+
+// 2
+for (i = 5; i <= 25; i += 4){
+    console.log(i);
+}
+
+// 3a
+const wizards = [
+    "Harry Potter",
+    "Hermoine Granger",
+    "Ron Weasley"
+]
+
+// 3b
+for (item of wizards){
+    console.log(item);
+}
+
+// 4a, 4b, 4c
+
+let harryPotterMovies = 0;
+while (harryPotterMovies <= 8) {
+  console.log(harryPotterMovies);
+  harryPotterMovies++;
+}
+
+// BONUS
